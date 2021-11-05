d41f137d35b146ffe624afaf0869ddda9d5da707
Wangdi Tenzing <wangdi457@gmail.com>
2021-11-05 11:25:53 -0400

    pushing agagin

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.use(express.static("public"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.js"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
